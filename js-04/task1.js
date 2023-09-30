9a50f9b566d6b60f340e7e64d0893bb97bf37517
Ramaz <romzess722@gmail.com>
2023-09-30 23:02:44 +0200

    add

--- js-04/task1.js
+++ js-04/task1.js
@@ -1,61 +1,52 @@
-function checkDeversant(arg) {
-  if (
-    arg === "Росія" ||
-    arg === "Россия" ||
-    arg === "Путін" ||
-    arg === "Путин"
-  ) {
-    alert("Fuch you!!!");
-    while (true) {}
-  }
-}
+const img = document.querySelector(".img");
+img.addEventListener("click", test);
 
-function runTest(qustions, answers) {
+function test() {
+  const qustion = [
+    "батько наш - Бандера,...",
+    "На що збирав гроші Притула?",
+    "Як українсько-англійським суржиком привітатися з народом",
+    "Слава",
+    "Слава",
+    "Смерть",
+    "Україна",
+  ];
+
+  const answers = [
+    "Україна - мати",
+    "на байрактари",
+    "Добрий день everybody",
+    "Україні",
+    "нації",
+    "ворогам",
+    "понад усе!",
+  ];
   let errorCounter = 0;
 
   for (let i = 0; i < 7; i = i + 1) {
-    const res = prompt(qustions[i]);
+    const res = prompt(qustion[i]);
 
-    checkDeversant(res);
+    if (
+      res === "Росія" ||
+      res === "Россия" ||
+      res === "Путін" ||
+      res === "Путин"
+    ) {
+      alert("Fuch you!!!");
+      while (true) {}
+    }
 
     if (res !== answers[i]) {
       if (errorCounter > 0) {
+        alert("Тест не пройденно!!!");
         break;
-      } else {
-        errorCounter = errorCounter + 1;
-        alert(`Ти помилився ${errorCounter} раз, більше неможна!!!`);
       }
+      errorCounter = errorCounter + 1;
+      alert(`У тебе ${errorCounter} помилка, помилятись більше не можна!!!`);
     }
   }
-}
 
-const qustionsUA = [
-  "батько наш - Бандера,...",
-  "На що збирав гроші Притула?",
-  "Як українсько-англійським суржиком вітався Джонсон",
-  "Слава",
-  "Слава",
-  "Смерть",
-  "Україна",
-];
-
-const answersUA = [
-  "Україна - мати",
-  "на байрактари",
-  "Добрий день everybody",
-  "Україні",
-  "нації",
-  "ворогам",
-  "понад усе",
-];
-
-function openTest() {
-  runTest(qustionsUA, answersUA);
-  alert("Ти справжній українець!!!");
-}
-
-const img = document.querySelector(".img");
-img.addEventListener("click", openTest);
-if (errorCounter === 0) {
-  alert("Ти справжній українець!!!");
+  if (errorCounter === 0) {
+    alert("Ти справжній українець");
+  }
 }
