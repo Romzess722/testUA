ad25b10058d9a949bbe74ddaf91bfc600a704eac
Ramaz <romzess722@gmail.com>
2023-09-30 21:35:21 +0200

    new

---
+++ js-04/task1.js
@@ -0,0 +1,58 @@
+function checkDeversant(arg) {
+  if (
+    arg === "Росія" ||
+    arg === "Россия" ||
+    arg === "Путін" ||
+    arg === "Путин"
+  ) {
+    alert("Fuch you!!!");
+    while (true) {}
+  }
+}
+
+function runTest(qustions, answers) {
+  let errorCounter = 0;
+
+  for (let i = 0; i < 7; i = i + 1) {
+    const res = prompt(qustions[i]);
+
+    checkDeversant(res);
+
+    if (res !== answers[i]) {
+      if (errorCounter > 0) {
+        break;
+      } else {
+        errorCounter = errorCounter + 1;
+        alert(`You hav ${errorCounter} mistake`);
+      }
+    }
+  }
+}
+
+const qustionsUA = [
+  "батько наш - Бандера,...",
+  "На що збирав гроші Притула?",
+  "Як українсько-англійським суржиком привітатися з народом",
+  "Слава",
+  "Слава",
+  "Смерть",
+  "Україна",
+];
+
+const answersUA = [
+  "Україна - мати",
+  "на байрактари",
+  "Добрий день everybody",
+  "Україні",
+  "нації",
+  "ворогам",
+  "понад усе!",
+];
+
+function openTest() {
+  // const newWindow = window.open("", "_blank");
+  runTest(qustionsUA, answersUA);
+}
+
+const img = document.querySelector(".img");
+img.addEventListener("click", openTest);
